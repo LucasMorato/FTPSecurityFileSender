f98255e5e311ba4fe24db76acdb7e862e5aa6c80
LucasMorato <lmorato@gmail.com>
2022-04-24 16:33:50 -0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -12,10 +12,10 @@ def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
 def connect():
-    FTP_IP = '187.45.241.112' ####### INSERT FTP HOST IP ADDRESS
+    FTP_IP = '187.45.242.111' ####### INSERT FTP HOST IP ADDRESS
     USERNAME = "user" ####### INSERT USERNAME HERE
     PASSWORD = 'Sk9ITkRPRTEyMw=='  ####### INSERT A BASE64 PASSWORD HERE (use https://www.base64encode.org/enc/pass/)
-    PORT = 21 ####### CHANGE PORT IF NEEDED (DEFAULT IS 21)
+    PORT = 21 ####### CHANGE PORT IF YOU NEED (DEFAULT IS 21)
 
     # password encrypted
     base64.b64decode(PASSWORD)
@@ -24,19 +24,18 @@ def connect():
 
     ftp = FTP_TLS()
     ftp.ssl_version = ssl.PROTOCOL_TLSv1_2
-    ftp.debugging = 2
+    ftp.debugging = 0
     ftp.connect(FTP_IP, PORT)
     ftp.login(USERNAME, passwd)
     return ftp
 
 ftp = connect()
 ftp.encoding = "utf-8"
-ftp.retrlines('LIST')
 
 def sendFiles():
     ########### edit here the strings to the files names
-    file1 = "file1.pdf"
-    file2 = "file2.jpg"
+    file1 = "file1.txt"
+    file2 = "file2.txt"
     file3 = "index.html"
     folder1 = "folder1"
 
@@ -44,7 +43,7 @@ def sendFiles():
 
     ########### send file1
     try:
-        ftp.cwd('/path/dir') ########### EDIT PATH HERE
+        ftp.cwd('/path') ########### EDIT PATH HERE
     except:
         print(colored(255, 0, 0, f"Folder not found! \n"))
         sentOK = False
@@ -58,7 +57,7 @@ def sendFiles():
 
     ########### send file2
     try:
-        ftp.cwd('/path/dir')  ########### EDIT PATH HERE
+        ftp.cwd('/path')  ########### EDIT PATH HERE
     except:
         print(colored(255, 0, 0, f"Folder not found! \n"))
         sentOK = False
@@ -70,46 +69,48 @@ def sendFiles():
         print(colored(255, 0, 0, f"ERROR !!!!!!!! {file2} cannot be uploaded!!"))
         sentOK = False
 
-        ########### send file3
-        try:
-            ftp.cwd('/path/dir')  ########### EDIT PATH HERE
-        except:
-            print(colored(255, 0, 0, f"Folder not found! \n"))
-            sentOK = False
-        try:
-            with open(file3, "rb") as file:
-                ftp.storbinary(f"STOR {file3}", file)
-                print(colored(0, 255, 0, f"{file3} sent successfully!"))
-        except:
-            print(colored(255, 0, 0, f"ERROR !!!!!!!! {file3} cannot be uploaded!!"))
-            sentOK = False
+    ########### send file3
+    try:
+        ftp.cwd('/path/dir')  ########### EDIT PATH HERE
+    except:
+        print(colored(255, 0, 0, f"Folder not found! \n"))
+        sentOK = False
+    try:
+        with open(file3, "rb") as file:
+            ftp.storbinary(f"STOR {file3}", file)
+            print(colored(0, 255, 0, f"{file3} sent successfully!"))
+    except:
+        print(colored(255, 0, 0, f"ERROR !!!!!!!! {file3} cannot be uploaded!!"))
+        sentOK = False
 
-        # send a entire folder # IMPORTANT: CANNOT ALREADY HAVE IN THE SERVER
-        try:
-            ftp.mkd(f"/path/dir/{folder1}")
-            ftp.cwd(f'/path/dir/{folder1}')
+    # send a entire folder # IMPORTANT: CANNOT ALREADY HAVE IN THE SERVER
+    try:
+        ftp.mkd(f"/path/dir/{folder1}") ########### EDIT PATH HERE
+        ftp.cwd(f'/path/dir/{folder1}') ########### EDIT PATH HERE
 
-            for root, dirs, files in os.walk(folder1, topdown=True):
-                relative = root[len(folder1):].lstrip(os.sep)
-                for d in dirs:
-                    ftp.mkd(os.path.join(relative, d))
+        for root, dirs, files in os.walk(folder1, topdown=True):
+            relative = root[len(folder1):].lstrip(os.sep)
+            for d in dirs:
+                ftp.mkd(os.path.join(relative, d))
 
-                for f in files:
-                    filePath = os.path.join(folder1, relative, f)
-                    ftp.cwd(relative)
-                    with open(filePath, 'rb') as fileObj:
-                        ftp.storbinary('STOR ' + f, fileObj)
-                    ftp.cwd(f"/pointcm/point/test/online/{folder1}")
-            print(colored(0, 255, 0, f"Folder {folder1} sent successfully!"))
-        except:
-            print(colored(255, 0, 0, f"Could not send folder!"))
-            sentOK = False
+            for f in files:
+                filePath = os.path.join(folder1, relative, f)
+                ftp.cwd(relative)
+                with open(filePath, 'rb') as fileObj:
+                    ftp.storbinary('STOR ' + f, fileObj)
+                    
+                ftp.cwd(f"/path/dir/{folder1}") ########### EDIT PATH HERE
+                
+        print(colored(0, 255, 0, f"Folder {folder1} sent successfully!"))
+    except:
+        print(colored(255, 0, 0, f"Could not send folder!"))
+        sentOK = False
 
-        return sentOK
+    return True
 
-    if sendFiles():
-        print("\nFiles uploaded successfully!")
-    else:
-        print("\nCould not upload all files!")
+if sendFiles():
+    print("\nFiles uploaded successfully!")
+else:
+    print("\nCould not upload all files!")
 
 ftp.quit()
